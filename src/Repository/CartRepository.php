3e0134a7897c17938a983f66f58265e054958f7d
Aleksei Udodov <weberdster@gmail.com>
2025-10-07 22:37:03 +0400

    added CartRepository

---
+++ src/Repository/CartRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+use Raketa\BackendTestTask\Domain\Cart;
+
+use Raketa\BackendTestTask\Infrastructure\CartTransportInterface;
+
+final class CartRepository
+{
+    public function __construct(
+        private CartTransportInterface $transport
+    ) {}
+
+    public function get(string $key): Cart
+    {
+        return $this->transport->get($key);
+    }
+
+    public function set(Cart $value, string $key): void
+    {
+        $this->transport->set($key, $value);
+    }
+
+    public function has($key): bool
+    {
+        return $this->transport->has($key);
+    }
+}
