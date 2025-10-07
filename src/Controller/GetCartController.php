dcbad6b1fcf285aabe6551e8ba0d5289d4f6ad75
Aleksei Udodov <weberdster@gmail.com>
2025-10-07 21:39:21 +0400

    added exception handling

--- src/Controller/GetCartController.php
+++ src/Controller/GetCartController.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Raketa\BackendTestTask\Controller;
 
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Raketa\BackendTestTask\Repository\CartManager;
 use Raketa\BackendTestTask\View\CartView;
+use Raketa\BackendTestTask\Controller\JsonResponse;
+use Raketa\BackendTestTask\Exception\CartNotFoundException;
+use Exception;
 
 final readonly class GetCartController
 {
@@ -19,31 +21,28 @@ final readonly class GetCartController
 
     public function __invoke(): ResponseInterface
     {
-        $response = new JsonResponse();
-        $cart = $this->cartManager->getCart();
+        try {
+            $response = new JsonResponse();
+            $cart = $this->cartManager->getCart();
 
-        if ($cart) {
             $response->getBody()->write(
                 json_encode(
                     $this->cartView->toArray($cart),
-                    JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                    JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
                 )
             );
 
             return $response
                 ->withHeader('Content-Type', 'application/json; charset=utf-8')
                 ->withStatus(200);
-        } else {
-            $response->getBody()->write(
-                json_encode(
-                    ['message' => 'Cart not found'],
-                    JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-                )
-            );
-
+        } catch (CartNotFoundException $e) {
             return $response
                 ->withHeader('Content-Type', 'application/json; charset=utf-8')
-                ->withStatus(404);
+                ->withStatus(404, 'Cart not found');
+        } catch (Exception $e) {
+            return $response
+                ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                ->withStatus(500, 'Internal server error');   
         }
     }
 }
