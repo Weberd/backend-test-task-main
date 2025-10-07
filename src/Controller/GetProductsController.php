447f0faea9b3c2c0930b6a1b18acc5ebbb86e86e
Aleksei Udodov <weberdster@gmail.com>
2025-10-07 21:39:39 +0400

    added exception handling

--- src/Controller/GetProductsController.php
+++ src/Controller/GetProductsController.php
@@ -7,6 +7,8 @@ namespace Raketa\BackendTestTask\Controller;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Raketa\BackendTestTask\View\ProductsView;
+use Raketa\BackendTestTask\Exception\ProductNotFoundException;
+use Exception;
 
 final readonly class GetProductsController
 {
@@ -17,9 +19,9 @@ final readonly class GetProductsController
 
     public function __invoke(RequestInterface $request): ResponseInterface
     {
-        $response = new JsonResponse();
-
-        $rawRequest = json_decode($request->getBody()->getContents(), true);
+        try {
+            $response = new JsonResponse();
+            $rawRequest = json_decode($request->getBody()->getContents(), true);
 
             if ($rawRequest === null || !isset($rawRequest['category'])) {
                 return $response
@@ -27,15 +29,20 @@ final readonly class GetProductsController
                     ->withStatus(400, 'Missing category parameter');            
             }
 
-        $response->getBody()->write(
-            json_encode(
-                $this->productsVew->toArray($rawRequest['category']),
-                JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-            )
-        );
+            $response->getBody()->write(
+                json_encode(
+                    $this->productsVew->toArray($rawRequest['category']),
+                    JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+                )
+            );
 
-        return $response
-            ->withHeader('Content-Type', 'application/json; charset=utf-8')
-            ->withStatus(200);
+            return $response
+                ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                ->withStatus(200);
+        } catch (Exception $e) {
+            return $response
+                ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                ->withStatus(500, 'Internal server error');   
+        }
     }
 }
