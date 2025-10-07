4147e7cbc0251ce6ba7376622dbaef7dd017ddf6
Aleksei Udodov <weberdster@gmail.com>
2025-10-07 22:35:03 +0400

    feat: add SessionService for session management

---
+++ src/Service/SessionService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Raketa\BackendTestTask\Service;
+
+final class SessionService
+{
+    public static function getId(): string {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        return session_id();
+    }
+}
