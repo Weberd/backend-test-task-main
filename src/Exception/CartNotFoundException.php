05804b3e12ce418bc64f8a7f63cdbb585eaf433c
Aleksei Udodov <weberdster@gmail.com>
2025-10-08 17:55:31 +0400

    removed not needed constructor

--- src/Exception/CartNotFoundException.php
+++ src/Exception/CartNotFoundException.php
@@ -1,13 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Raketa\BackendTestTask\Exception;
 
 use Exception;
 
 class CartNotFoundException extends Exception
 {
-    public function __construct(string $message)
-    {
-        parent::__construct($message);
-    }
 }
--- src/Exception/CartTransportException.php
+++ src/Exception/CartTransportException.php
@@ -8,8 +8,4 @@ use Exception;
 
 class CartTransportException extends Exception
 {
-    public function __construct(string $message = "", int $code = 0, ?Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
